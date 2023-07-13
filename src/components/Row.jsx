87af358d6c61e464fe78eef552cfdbd1133efdf7
matheusnsilva7 <silva.matheus6081@gmail.com>
2023-07-13 13:44:55 -0300

    group4

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,17 +1,17 @@
 
 const Row = ({ alumno }) => {
-  return (
-    <tr>
-      <th scope="row">{alumno.id}</th>
-      <td>{alumno.name}</td>
-      <td>{alumno.lastName}</td>
-      <td>{alumno.grade}</td>
-      <td>{alumno.age}</td>
-      <td>
-        <button className="delete">X</button>
-      </td>
-    </tr>
-  )
-}
-
-export default Row
+    return (
+      <tr>
+        <th scope="row">{alumno.id}</th>
+        <td>{alumno.name}</td>
+        <td>{alumno.lastName}</td>
+        <td>{alumno.grade}</td>
+        <td>{alumno.age}</td>
+        <td>
+          <button className="delete"> X </button>
+        </td>
+      </tr>
+    )
+  }
+  
+  export default Row
